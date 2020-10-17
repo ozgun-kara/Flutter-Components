421fb6a3f41b495672cf1015abffbac97c04d885
Ozgun KARA <ozgunkara.devp@gmail.com>
2020-10-17 18:39:14 +0300

    Application settings are made

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -122,9 +122,9 @@ const ColorScheme _shrineColorScheme = ColorScheme(
 const Color shrinePink50 = Color(0xFFFEEAE6);
 const Color shrinePink100 = Color(0xFFFEDBD0);
 const Color shrinePink300 = Color(0xFFFBB8AC);
-const Color shrinePink400 = Color(0xFFEAA4A4);
+const Color shrinePink400 = Color(0xFAEAA4A4);
 
-const Color shrineBrown900 = Color(0xFF442B2D);
+const Color shrineBrown900 = Color(0xDF442B2D);
 const Color shrineBrown600 = Color(0xFF7D4F52);
 
 const Color shrineErrorRed = Color(0xFFC5032B);
