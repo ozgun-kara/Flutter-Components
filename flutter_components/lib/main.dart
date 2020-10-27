fb01010f2eada6e3a046418df1ff7470e0532a4a
Ozgun KARA <ozgunkara.devp@gmail.com>
2020-10-27 15:30:14 +0300

    Application settings are made

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -204,13 +205,9 @@ const Color shrinePink50 = Color(0xFFFEEAE6);
 const Color shrinePink100 = Color(0xFFFEDBD0);
 const Color shrinePink300 = Color(0xFFFBB8AC);
 const Color shrinePink400 = Color(0xFFEAA4A4);
-
 const Color shrineBrown900 = Color(0xFF442B2D);
 const Color shrineBrown600 = Color(0xFF7D4F52);
-
 const Color shrineErrorRed = Color(0xFFC5032B);
-
 const Color shrineSurfaceWhite = Color(0xFFFFFBFA);
 const Color shrineBackgroundWhite = Colors.white;
-
 const defaultLetterSpacing = 0.03;
