ffd834ae5bd7c4d78e575ffb68815d70af1f2e65
ozgun-kara <ozgunkara.devp@gmail.com>
2020-11-30 16:48:47 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -29,6 +29,7 @@ class MyHomePage extends StatefulWidget
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> 
 {
   Future<List<User>> _getUsers() async 
