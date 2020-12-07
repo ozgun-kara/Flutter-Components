795604fc96bcec4e789cc12760d8718d7b460b0d
ozgun-kara <ozgunkara.devp@gmail.com>
2020-12-07 10:29:56 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -62,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage>
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
+
 	    
       body: SafeArea(
         child: Center(
