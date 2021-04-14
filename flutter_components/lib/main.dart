823efc7f244ccabad8a643e56b72b4723b5baaff
ozgun-kara <ozgunkara.devp@gmail.com>
2021-04-14 14:55:24 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'dart:async';
 
@@ -30,6 +29,7 @@ class MyHomePage extends StatefulWidget
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
+
 
 
 class _MyHomePageState extends State<MyHomePage> 
