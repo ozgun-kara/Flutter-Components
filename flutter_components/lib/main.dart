aff13fc356158e92b56361278e62532108e5da4a
ozgun-kara <ozgunkara.devp@gmail.com>
2021-04-27 17:31:03 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -20,6 +20,19 @@ class MyApp extends StatelessWidget
   }
 }
 
+  @override
+  Widget build(BuildContext context) 
+  {
+	return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Users'),
+    );
+  }
+}
+
 
 class MyHomePage extends StatefulWidget 
 {
