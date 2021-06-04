589ff0bd8278887fb716814db0d44039a5b5582c
ozgun-kara <ozgunkara.devp@gmail.com>
2021-06-04 17:03:53 +0300

    Update card.dart

--- flutter_components/lib/card.dart
+++ flutter_components/lib/card.dart
@@ -22,9 +22,6 @@ class ExpansionCard extends StatefulWidget {
   }) : assert(initiallyExpanded != null),
         super(key: key);
 
-
-
-
   /// The primary content of the list item.
   ///
   /// Typically a [Text] widget.
