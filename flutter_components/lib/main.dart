79dde9968b85a1ce85453447b2f497c2e73d7d52
ozgun-kara <ozgunkara.devp@gmail.com>
2021-06-27 16:42:42 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -21,8 +21,15 @@ class MyApp extends StatelessWidget
   }
 }
 
+class MyHomePage extends StatefulWidget 
+{
 
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
 
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
 
 class _MyHomePageState extends State<MyHomePage> 
 {
@@ -138,7 +145,6 @@ class DetailPage extends StatelessWidget
 
 class User 
 {
-
   final int index;
   final String email;
   final String picture;
