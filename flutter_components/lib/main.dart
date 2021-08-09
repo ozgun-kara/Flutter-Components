8802aacc721a14263069bf2f544f50a274b0c940
ozgun-kara <ozgunkara.devp@gmail.com>
2021-08-09 17:14:15 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -136,7 +136,12 @@ class DetailPage extends StatelessWidget
 
   @override
   Widget build(BuildContext context) 
-
+  {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text(user.name),
+    ));
+  }
 }
 
 class User 
