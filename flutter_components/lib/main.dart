a6cae4a04b342639fcb3a3d2c7ecb52129098d41
ozgun-kara <ozgunkara.devp@gmail.com>
2021-08-25 17:00:33 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -154,9 +154,6 @@ class User
 	
   final String name;
   final int index;
-  final String email;	
-  final String picture;
-  final String about;
 
 
   User(this.index, this.about, this.name, this.email, this.picture);
