48b5c8e6017b9090f3d1c4f22ff7f38cd85b715d
ozgun-kara <ozgunkara.devp@gmail.com>
2021-09-13 13:04:07 +0300

    Update card.dart

--- flutter_components/lib/card.dart
+++ flutter_components/lib/card.dart
@@ -64,9 +64,7 @@ class _ExpansionTileState extends State<ExpansionCard> with SingleTickerProvider
   final ColorTween _iconColorTween = ColorTween();
   final ColorTween _backgroundColorTween = ColorTween();
 
-	Animation<Color> _borderColor;
-    Animation<Color> _headerColor;
-    Animation<Color> _backgroundColor;
+
 	bool _isExpanded = false;
 	Animation<double> _heightFactor;
     AnimationController _controller;
