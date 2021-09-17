e3c9c1d0f10288afd52a363ba7ac276a541a37ca
ozgun-kara <ozgunkara.devp@gmail.com>
2021-09-17 17:24:32 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -150,7 +150,9 @@ class DetailPage extends StatelessWidget
 }
 
 class User 
-{   
+{     final int index;
+  final String email;	
+  final String picture;
   final String about
   final String name;
 
