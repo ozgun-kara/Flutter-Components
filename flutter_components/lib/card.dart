b416a42ec19d0068f1e8e260d566f88595a1498b
Ozgun <ozgunkara.devp@gmail.com>
2022-01-26 16:56:04 +0300

    .

--- flutter_components/lib/card.dart
+++ flutter_components/lib/card.dart
@@ -70,10 +70,7 @@ class _ExpansionTileState extends State<ExpansionCard> with SingleTickerProvider
   Animation<double> _heightFactor;
   Animation<Color> _headerColor;
   Animation<Color> _iconColor;
-  Animation<Color> _borderColor;
-    AnimationController _controller;
-  Animation<Color> _backgroundColor;
-  Animation<double> _iconTurns;
+
 
   @override
   void initState() {
