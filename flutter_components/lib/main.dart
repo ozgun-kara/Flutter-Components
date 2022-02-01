05b761c9df83044fe0d0d417572e946382aca466
Ozgun <ozgunkara.devp@gmail.com>
2022-02-01 09:12:06 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -8,6 +8,19 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget 
 { 
 
+   @override
+  Widget build(BuildContext context) 
+  {
+  return new MaterialApp(
+  
+   title: 'Flutter Demo',
+     
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Users'),
+    );
+  }
 
 
 }
@@ -138,11 +151,7 @@ class DetailPage extends StatelessWidget
 class User 
 {    
 
-  final String email;
-  final String picture;
-  final String about    
-  final String name;
-  final int index;
+
 
 
   User(this.index, this.about, this.name, this.email, this.picture);
