b76f9f19b8785af2b23022b01f600dac76ff584a
ozgun-kara <ozgunkara.devp@gmail.com>
2022-03-06 19:49:32 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -1,12 +1,8 @@
 
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-
-
 
 void main() => runApp(new MyApp());
 
@@ -25,8 +21,6 @@ class MyApp extends StatelessWidget
       home: new MyHomePage(title: 'Users'),
     );
   }
-
-
 
 }
 
