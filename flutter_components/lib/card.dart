1423f4a89ed4a7ce75f21dfc2b397d50b2c32a0a
ozgun-kara <ozgunkara.devp@gmail.com>
2022-03-09 16:28:47 +0300

    Update card.dart

--- flutter_components/lib/card.dart
+++ flutter_components/lib/card.dart
@@ -8,6 +8,9 @@ class ExpansionCard extends StatefulWidget {
 
  const ExpansionCard({
 
+    @required this.title,
+	this.onExpansionChanged,
+    Key key,
 
 
   }) : assert(initiallyExpanded != null),
