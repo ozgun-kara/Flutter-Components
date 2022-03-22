25c3aa422f796c4704c859b4bc93b6dc0d1134cd
ozgun-kara <ozgunkara.devp@gmail.com>
2022-03-22 13:27:06 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -6,6 +6,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 
+class MyApp extends StatelessWidget 
+{ 
+   @override
+  Widget build(BuildContext context) 
+  {
+  return new MaterialApp(
+  
+   title: 'Flutter Demo',
+     
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Users'),
+    );
+  }
+
+}
 
 class MyHomePage extends StatefulWidget 
 {
