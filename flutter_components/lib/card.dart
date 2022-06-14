06b80ab337484f236682e259a7a999851b9e7947
ozgun-kara <ozgunkara.devp@gmail.com>
2022-06-14 15:03:54 +0300

    Update card.dart

--- flutter_components/lib/card.dart
+++ flutter_components/lib/card.dart
@@ -80,7 +80,18 @@ class _ExpansionTileState extends State<ExpansionCard> with SingleTickerProvider
   void initState() {
     super.initState();
 	
+    _controller = AnimationController(duration: _kExpand, vsync: this);
+    _heightFactor = _controller.drive(_easeInTween);
+	_iconTurns = _controller.drive(_halfTween.chain(_easeInTween));
+	_borderColor = _controller.drive(_borderColorTween.chain(_easeOutTween));
+    _headerColor = _controller.drive(_headerColorTween.chain(_easeInTween));
 
+	_iconColor = _controller.drive(_iconColorTween.chain(_easeInTween));
+    _backgroundColor = _controller.drive(_backgroundColorTween.chain(_easeOutTween));
+
+    _isExpanded = PageStorage.of(context)?.readState(context) ?? widget.initiallyExpanded;
+    if (_isExpanded)
+      _controller.value = 1.0;
 
   }
 
