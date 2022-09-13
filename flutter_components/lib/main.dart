e9a604f621a641873714822642022ef23be3343f
ozgun-kara <ozgunkara.devp@gmail.com>
2022-09-13 11:48:05 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -8,19 +8,23 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget 
 { 
   
-
+     @override
+  Widget build(BuildContext context) 
+  {
+  return new MaterialApp(
+  
+   title: 'Flutter Demo',
+     
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Users'),
+    );
+  }
   
 }
 
-class MyHomePage extends StatefulWidget 
-{
 
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
 
 
 class _MyHomePageState extends State<MyHomePage> 
