a65ba07e41bc80bf0a4e4eaea86313fc8c42a7b0
ozgun-kara <ozgunkara.devp@gmail.com>
2023-06-11 13:11:37 +0300

    Update card.dart

--- flutter_components/lib/card.dart
+++ flutter_components/lib/card.dart
@@ -139,13 +139,7 @@ class _ExpansionTileState extends State<ExpansionCard> with SingleTickerProvider
 
       Container(
 
-    decoration: BoxDecoration(
-          color: _backgroundColor.value ?? Colors.transparent,
-          border: Border(
-            top: BorderSide(color: borderSideColor),
-            bottom: BorderSide(color: borderSideColor),
-          ),
-        ),
+
 
         child: Column(
           mainAxisSize: MainAxisSize.min,
