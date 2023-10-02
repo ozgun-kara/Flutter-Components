87739df388db5dae8d831f74f97f36b41071ad88
ozgun-kara <ozgunkara.devp@gmail.com>
2023-10-02 13:17:34 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -39,26 +39,6 @@ class _MyHomePageState extends State<MyHomePage>
 {
 
 
-  Future<List<User>> _getUsers() async 
-  {
-    var data = await http.get("http://www.json-generator.com/api/json/get/bQlOGsODVK?indent=2");
-    var jsonData = json.decode(data.body);
-
-    List<User> users = [];
-  
-      for (var u in jsonData) 
-      {
-      User user =
-      User(u["index"], u["about"], u["name"], u["email"], u["picture"]);
-      
-      for (int i = 0; i < 50; i++)
-      {
-        users.add(user);
-      }
-      }
-
-    return users;
-  }
 
 
   @override
@@ -154,10 +134,8 @@ class DetailPage extends StatelessWidget
   } 
 }
 
-
 class User 
 {    
-
   final String about   
   final String name;
   final int index;
