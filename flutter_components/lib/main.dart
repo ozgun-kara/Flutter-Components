c10cab527fd1b10eaba89a2ab973b7d91a254449
ozgun-kara <ozgunkara.devp@gmail.com>
2023-10-28 13:47:18 +0300

    Update main.dart

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -137,15 +137,19 @@ class _MyHomePageState extends State<MyHomePage>
   }
 }
 
+class DetailPage extends StatelessWidget 
+{
 
+  final User user;
+  DetailPage(this.user);
 
-class User 
-{    
-  final String about   
-  final String name;
-  final int index;
-  final String email;
-  final String picture;
-
-  User(this.index, this.about, this.name, this.email, this.picture);
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text(user.name),
+    ));
+  } 
 }
+
